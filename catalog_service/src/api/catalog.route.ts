a12798ceb16ad7bf8747ef8aced53384688da1b2
Agus27111 <agussetiawanbelajar@gmail.com>
2024-11-02 00:44:29 +0700

    add test for route catalog

--- catalog_service/src/api/catalog.route.ts
+++ catalog_service/src/api/catalog.route.ts
@@ -1,17 +1,51 @@
 import express, { NextFunction, Request, Response } from "express";
+import { CatalogService } from "../services/catalog.service";
+import { CatalogRepository } from "../repository/catalog.repository";
+import { RequestValidator } from "../utils/requestValidator";
+import { CreateProductRequest } from "../dto/product.dto";
 
 const router = express.Router();
 
-const asyncHandler = (fn: Function) => (req: Request, res: Response, next: NextFunction) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
+export const catalogService = new CatalogService(new CatalogRepository());
 
 // endpoints
 router.post(
-  "/product",
-  asyncHandler(async (req: Request, res: Response) => {
-    return res.status(201).json({});
-  })
+  "/products",
+  async (req: Request, res: Response, _next: NextFunction) => {
+    try {
+      const { errors, input } = await RequestValidator(
+        CreateProductRequest,
+        req.body
+      )
+
+      if (errors) return res.status(400).json(errors);
+      const data = await catalogService.createProduct(input);
+      return res.status(201).json(data);
+    } catch (error) {
+      console.log(error);
+      const err = error as Error;
+      return res.status(500).json(err.message);
+    }
+  }
+);
+
+router.patch(
+  "/products/:id",
+  async (req: Request, res: Response, _next: NextFunction) => {
+    try {
+      const { errors, input } = await RequestValidator(
+        CreateProductRequest,
+        req.body
+      );
+
+      if (errors) return res.status(400).json(errors);
+      const data = await catalogService.createProduct(input);
+      return res.status(201).json(data);
+    } catch (error) {
+      const err = error as Error;
+      return res.status(500).json(err.message);
+    }
+  }
 );
 
 export default router;
